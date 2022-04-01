82201768bbb09df8fcd93acfd53964a97a694ff5
Eric Sakmar <esakmar@truefit.io>
2022-04-01 09:38:32 -0400

    fixes dates

--- src/components/event.js
+++ src/components/event.js
@@ -3,13 +3,15 @@ import { format } from "date-fns"
 
 const formatTime = date => format(date, "h:mm aaa")
 
-const Event = ({ event }) => (
-  <div>
-    <h3>{event.title}</h3>
+const Event = ({ event }) => {
+  return (
+    <div>
+      <h3>{event.title}</h3>
 
-    <p>{event.location}</p>
-    <p>{formatTime(event.date)}</p>
-  </div>
-)
+      <p>{event.location}</p>
+      <p>{formatTime(event.date)}</p>
+    </div>
+  )
+}
 
 export default Event
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import { format, startOfDay } from "date-fns"
+import { compareAsc, format, isAfter, startOfDay } from "date-fns"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Day from "../components/day"
 
 const IndexPage = ({ data }) => {
-  const today = startOfDay(new Date()).getTime()
+  const today = startOfDay(new Date())
 
   const grouped = data.allEvents.edges
     .map(e => e.node)
-    .map(e => ({ ...e, date: parseInt(e.date, 10) }))
-    .filter(e => e.date >= today)
     .map(e => ({ ...e, date: new Date(e.date) }))
+    .filter(e => isAfter(e.date, today))
+    .concat()
+    .sort((a, b) => compareAsc(a.date, b.date))
     .reduce((groups, e) => {
       const key = format(e.date, "yyyy-MM-dd")
       const events = groups[key] ?? []
