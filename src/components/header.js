be3971945d6ea93f12beeaf099303f9db9dad597
Eric Sakmar <esakmar@truefit.io>
2022-04-01 12:10:31 -0400

    fancy header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,14 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 
-const Header = ({ siteTitle }) => (
-  <header style={{ textAlign: "center" }}>
-    <div
-      style={{
-        padding: `1.45rem 1.0875rem`
-      }}
-    >
-      <h1>pgh.events</h1>
-    </div>
+import * as styles from "./header.module.css"
+
+const Header = () => (
+  <header>
+    <h1 className={styles.header}>
+      <div className={styles.name}>pgh</div>
+      <div>events</div>
+    </h1>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,26 +25,16 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header />
 
       <div
         style={{
           margin: `0 auto`,
           maxWidth: "45rem",
-          padding: `0 1.0875rem 1.45rem`
+          padding: `0 1rem 1rem`
         }}
       >
         <main>{children}</main>
-
-        <footer
-          style={{
-            marginTop: `2rem`
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
       </div>
     </>
   )
