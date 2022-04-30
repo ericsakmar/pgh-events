0e7298c2d14a6146109974f902003714053c1b4b
Eric Sakmar <esakmar@truefit.io>
2022-04-30 11:37:17 -0400

    meta stuff

--- src/components/seo.js
+++ src/components/seo.js
@@ -11,7 +11,7 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 function Seo({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
+  const { site, ogImageDefault } = useStaticQuery(
     graphql`
       query {
         site {
@@ -21,53 +21,68 @@ function Seo({ description, lang, meta, title }) {
             author
           }
         }
+
+        ogImageDefault: file(relativePath: { eq: "card.png" }) {
+          childImageSharp {
+            fixed(height: 1200, width: 1200) {
+              src
+            }
+          }
+        }
       }
     `
   )
 
+  const cardImageSrc = `https://pgh.events${ogImageDefault.childImageSharp.fixed.src}`
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
 
   return (
     <Helmet
       htmlAttributes={{
-        lang,
+        lang
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
         {
           name: `description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:title`,
-          content: title,
+          content: title
         },
         {
           property: `og:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `website`
+        },
+        {
+          property: `og:image`,
+          content: cardImageSrc
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary`
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
+          content: site.siteMetadata?.author || ``
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: title
         },
         {
           name: `twitter:description`,
-          content: metaDescription,
+          content: metaDescription
         },
+        {
+          name: `twitter:image`,
+          content: cardImageSrc
+        }
       ].concat(meta)}
     />
   )
@@ -76,14 +91,14 @@ function Seo({ description, lang, meta, title }) {
 Seo.defaultProps = {
   lang: `en`,
   meta: [],
-  description: ``,
+  description: ``
 }
 
 Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 }
 
 export default Seo
