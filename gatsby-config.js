ed42e20d81a9215d7f9f60b231e95f707a51f017
Eric Sakmar <esakmar@truefit.io>
2022-05-04 13:30:59 -0400

    maskable icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: `pgh.events`,
     description: `pittsburgh events`,
     author: `https://github.com/ericsakmar`,
-    siteUrl: `https://pgh.events`,
+    siteUrl: `https://pgh.events`
   },
 
   plugins: [
@@ -16,8 +16,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -32,11 +32,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/pgh-events.png`, // This path is relative to the root of the site.
         icon_options: {
-          purpose: `maskable`,
-        },
-      },
+          purpose: `any maskable`
+        }
+      }
     },
     `gatsby-plugin-offline`,
-    "source-plugin",
-  ],
+    "source-plugin"
+  ]
 }
