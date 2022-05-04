ba8951c3aaad47260560e69a6b78ece1a4287280
Eric Sakmar <esakmar@truefit.io>
2022-05-04 11:14:14 -0400

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: `pgh.events`,
     description: `pittsburgh events`,
     author: `https://github.com/ericsakmar`,
-    siteUrl: `https://pgh.events`
+    siteUrl: `https://pgh.events`,
   },
 
   plugins: [
@@ -16,8 +16,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -30,10 +30,13 @@ module.exports = {
         background_color: `#daa520`,
         theme_color: `#daa520`,
         display: `minimal-ui`,
-        icon: `src/images/pgh-events.png` // This path is relative to the root of the site.
-      }
+        icon: `src/images/pgh-events.png`, // This path is relative to the root of the site.
+        icon_options: {
+          purpose: `maskable`,
+        },
+      },
     },
     `gatsby-plugin-offline`,
-    "source-plugin"
-  ]
+    "source-plugin",
+  ],
 }
