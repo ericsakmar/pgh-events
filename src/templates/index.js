cfc1ca8a65527f727e3aa387f87f8732ce174afd
Eric Sakmar <esakmar@truefit.io>
2022-06-06 13:54:57 -0400

    got it

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,19 +6,32 @@ import Seo from "../components/seo"
 import Day from "../components/day"
 import Nav from "../components/nav"
 
-const IndexPage = ({ pageContext }) => {
-  const { events, currentPage, numPages } = pageContext
+const IndexPage = ({ pageContext, location }) => {
+  const { events, currentPage, numPages, allEvents } = pageContext
 
-  // TODO do we need to sort the date keys?
-  const content = Object.entries(events).map(([date, events]) => (
-    <Day key={date} date={date} events={events} />
-  ))
+  const searchParams = new URLSearchParams(location.search)
+  const searchDate = searchParams.get("d")
+  const state = searchDate ? "SEARCHING" : "DEFAULT"
+
+  // TODO empty state
+  const content =
+    state === "SEARCHING" ? (
+      <Day key={searchDate} date={searchDate} events={allEvents[searchDate]} />
+    ) : (
+      Object.entries(events).map(([date, events]) => (
+        <Day key={date} date={date} events={events} />
+      ))
+    )
 
   return (
     <Layout>
       <Seo title="pgh.events" />
+
       {content}
-      <Nav currentPage={currentPage} numPages={numPages} />
+
+      {state === "DEFAULT" ? (
+        <Nav currentPage={currentPage} numPages={numPages} />
+      ) : null}
     </Layout>
   )
 }
