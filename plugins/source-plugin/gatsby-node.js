e57b7a84ef41ba264ecd2c02328d6a71e36dc9fd
Eric Sakmar <esakmar@truefit.io>
2022-10-07 12:11:22 -0400

    nlp test

--- plugins/source-plugin/gatsby-node.js
+++ plugins/source-plugin/gatsby-node.js
@@ -94,7 +94,7 @@ exports.sourceNodes = async ({
     warhol,
   ]
 
-  const devSources = [blackforge]
+  const devSources = [brillo]
 
   const sources =
     process.env.NODE_ENV === "development" ? devSources : prodSources
--- plugins/source-plugin/sources/brillo.js
+++ plugins/source-plugin/sources/brillo.js
@@ -1,10 +1,28 @@
 const cheerio = require("cheerio")
+const winkNLP = require("wink-nlp")
+const its = require("wink-nlp/src/its.js")
+const as = require("wink-nlp/src/as.js")
+const model = require("wink-eng-lite-model")
 const fetchDynamicPage = require("./fetchDynamicPage")
 const { parseDate } = require("./parseDate")
 
 const url = "https://www.brilloboxpgh.com/events/"
 const waitForSelector = ".eo-eb-event-box"
 exports.url = url
+
+const nlp = winkNLP(model)
+
+const findTime = text => {
+  const doc = nlp.readDoc(text)
+
+  const times = doc
+    .entities()
+    .filter(e => e.out(its.type) === "TIME")
+    .out()
+    .filter(t => t !== "NIGHT")
+
+  return times.length > 0 ? times[0] : null
+}
 
 exports.getEvents = async () => {
   const data = await fetchDynamicPage.fetchDynamicPage(url, waitForSelector)
@@ -16,30 +34,20 @@ exports.getEvents = async () => {
     .map(el => {
       const n = $(el)
 
-      const title = n
-        .find(".eo-eb-event-title")
-        .text()
-        .trim()
+      const title = n.find(".eo-eb-event-title").text().trim()
 
-      const rawDate = n
-        .find(".eo-eb-date-container")
-        .text()
-        .trim()
+      const rawDate = n.find(".eo-eb-date-container").text().trim()
 
-      const date = parseDate(rawDate)
+      const rawTime = findTime(title)
+      const hasTime = rawTime !== null
+
+      const date = parseDate(hasTime ? `${rawDate} at ${rawTime}` : rawDate)
 
       const location = "Brillobox"
 
-      const link = n
-        .find(".eo-eb-event-title a")
-        .attr("href")
-        .trim()
+      const link = n.find(".eo-eb-event-title a").attr("href").trim()
 
-      const poster = n
-        .find("img")
-        .attr("src")
-        .trim()
-        .replace("http", "https")
+      const poster = n.find("img").attr("src").trim().replace("http", "https")
 
       return {
         title,
@@ -47,8 +55,8 @@ exports.getEvents = async () => {
         location,
         link,
         source: url,
-        hasTime: false, // you may be able to get it from the event description
-        poster
+        hasTime,
+        poster,
       }
     })
 
