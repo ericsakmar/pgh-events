8301edda52d01e813ed50326c8490e16436a919f
Eric Sakmar <esakmar@truefit.io>
2024-10-13 09:17:37 -0400

    temporarily removes last build check

--- netlify/functions/scheduled-deploy.mjs
+++ netlify/functions/scheduled-deploy.mjs
@@ -1,15 +1,15 @@
 import fetch from "node-fetch"
-import { NetlifyAPI } from "netlify"
+// import { NetlifyAPI } from "netlify"
 
 const BUILD_HOOK = process.env.BUILD_HOOK
-const API_TOKEN = process.env.API_TOKEN
+// const API_TOKEN = process.env.API_TOKEN
 const HOUR_IN_MS = 1000 * 60 * 60
-const client = new NetlifyAPI(API_TOKEN)
+// const client = new NetlifyAPI(API_TOKEN)
 
 export default async () => {
-  const lastDeployDate = await getLastDeployDate()
-  const now = new Date()
-  const doIt = shouldBuild(now, lastDeployDate)
+  // const lastDeployDate = await getLastDeployDate()
+  // const now = new Date()
+  const doIt = true // shouldBuild(now, lastDeployDate)
 
   if (doIt) {
     console.log("sending request to build hook")
@@ -30,15 +30,15 @@ export const config = {
   schedule: "0 7 * * *",
 }
 
-const getLastDeployDate = async () => {
-  const deploys = await client.listSiteDeploys({
-    site_id: "eaa69b22-e0b2-4623-9532-fc58d5203e17",
-  })
-
-  return new Date(deploys[0].created_at)
-}
-
-const shouldBuild = (now, lastDeployDate) => {
-  const diff = now - lastDeployDate
-  return diff > HOUR_IN_MS
-}
+// const getLastDeployDate = async () => {
+//   const deploys = await client.listSiteDeploys({
+//     site_id: "eaa69b22-e0b2-4623-9532-fc58d5203e17",
+//   })
+//
+//   return new Date(deploys[0].created_at)
+// }
+//
+// const shouldBuild = (now, lastDeployDate) => {
+//   const diff = now - lastDeployDate
+//   return diff > HOUR_IN_MS
+// }
